7474587b8c2280e94b9261d4532ed454f5f4bdda
sdonich <sdonich@gmail.com>
2019-05-27 11:27:11 +0200

    some fix

--- src/components/FilterLink.jsx
+++ src/components/FilterLink.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 function FilterLink(props) {
 	return (
+		// eslint-disable-next-line
 		<a className={props.active ? 'is-active' : ''} onClick={props.onClick}>
 			<i className="material-icons">{props.icon}</i>
 		</a>
